e53608ab0700860c4db21815ab4dc0dc93e36d92
MonroeLionel <MonroeLionel@gmail.com>
2022-05-25 22:05:42 +0300

    qwe

---
+++ 17.js
@@ -0,0 +1,7 @@
+// Create Decimal Numbers with JavaScript
+
+/*
+Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+*/
+
+let myDecimal = 5.7;
---
+++ 2.js
@@ -0,0 +1,10 @@
+// Declare JavaScript Variables
+
+/*
+Use the var keyword to create a variable called myName.
+
+Hint
+Look at the ourName example above if you get stuck.
+*/
+
+var myName;
