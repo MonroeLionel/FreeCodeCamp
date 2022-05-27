9e32903dcd2b7d56e67803b3623512a496bff23b
MonroeLionel <MonroeLionel@gmail.com>
2022-05-27 20:48:18 +0300

    qwe

---
+++ 40.js
@@ -0,0 +1,8 @@
+// Nest one Array within Another Array
+
+/*
+Create a nested array called myArray.
+*/
+
+// Only change code below this line
+const myArray = [["text", 123, true], ["vasan", 99, false]];
