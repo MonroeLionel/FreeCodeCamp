802038ddd7776d0ecff926798f9110f5bb960d37
MonroeLionel <MonroeLionel@gmail.com>
2022-05-28 23:29:02 +0300

    qwe

---
+++ 45.js
@@ -0,0 +1,10 @@
+// Manipulate Arrays With pop()
+
+/*
+Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+*/
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
+// Only change code below this line
