e95e9bc5b11d63e970efebe63d087d95e880aa49
MonroeLionel <MonroeLionel@gmail.com>
2022-05-28 23:43:48 +0300

    qwe

---
+++ 51.js
@@ -0,0 +1,9 @@
+// Return a Value from a Function with Return
+
+/*
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+*/
+
+function timesFive(arg) {
+  return arg * 5;
+};
