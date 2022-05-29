6d87edf1a0d32b3a907ebb95e85ce14023c61902
MonroeLionel <MonroeLionel@gmail.com>
2022-05-29 22:40:30 +0300

    qwe

---
+++ 54.js
@@ -0,0 +1,16 @@
+// Global vs. Local Scope in Functions
+
+/*
+Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+*/
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  let outerWear = "sweater"
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
